0c11d4633b560692abc00f2c7c3a14e57ceba49c
gauravtechies <gauravkamboj13121992@gmail.com>
2020-03-13 13:01:43 +0530

    initial setup

---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// module.exports.hello = (event, context,callback)=>{
+//   const response={
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: 'Go Serverless v1.0! Your function executed successfully ',
+//       input: event,
+//     }),
+//   };
+  
+//   callback(null,response);
+//   // callback(new Error("Something went wrong"));
+// }; 
+
+// If we pass data from function and that we have to pass like [sls invoke local -f hello -d 'wut up world'] [ sls invoke local -f hello -d '{"first":2, "second":10}']
+module.exports.hello = (event, context,callback)=>{
+  const {first, second} =event;  // value in event comes from command line [sls invoke local -f hello -d '{"first":2, "second":10}'] and then it will get into {first, second}
+  const added =first + second  // then from object they are adding value 
+  const response={
+    statusCode: 200,
+    body: JSON.stringify({
+      // input: event,
+      event: added
+    }),
+  };
+  
+  callback(null,response);
+  // callback(new Error("Something went wrong"));
+}; 

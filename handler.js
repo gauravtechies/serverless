101415bde3c2eef0cf2aad285f5de37880029d54
gauravtechies <gauravkamboj13121992@gmail.com>
2020-03-13 17:18:51 +0530

    rest endpoint

--- handler.js
+++ handler.js
@@ -14,17 +14,42 @@
 // }; 
 
 // If we pass data from function and that we have to pass like [sls invoke local -f hello -d 'wut up world'] [ sls invoke local -f hello -d '{"first":2, "second":10}']
-module.exports.hello = (event, context,callback)=>{
-  const {first, second} =event;  // value in event comes from command line [sls invoke local -f hello -d '{"first":2, "second":10}'] and then it will get into {first, second}
-  const added =first + second  // then from object they are adding value 
-  const response={
-    statusCode: 200,
-    body: JSON.stringify({
-      // input: event,
-      event: added
-    }),
-  };
+// module.exports.hello = (event, context,callback)=>{
+//   const {first, second} =event;  // value in event comes from command line [sls invoke local -f hello -d '{"first":2, "second":10}'] and then it will get into {first, second}
+//   const added =first + second  // then from object they are adding value 
+//   const response={
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       // input: event,
+//       event: added
+//     }),
+//   };
   
-  callback(null,response);
-  // callback(new Error("Something went wrong"));
-}; 
+//   callback(null,response);
+//   // callback(new Error("Something went wrong"));
+// }; 
+
+// module.exports.hello = (event, context,callback)=>{
+//   const {first, second} =event;  // value in event comes from command line [sls invoke local -f hello -d '{"first":2, "second":10}'] and then it will get into {first, second}
+//   const added =first + second  // then from object they are adding value 
+//   const response={
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "Hello there"
+//     }),
+//   };
+  
+//   callback(null,response);
+//   // callback(new Error("Something went wrong"));
+// }; 
+  module.exports.hello = (event, context,callback)=>{
+    console.log(event.pathParameters.name);
+    const reply = `Hello ${event.pathParameters.name}`;
+      const response={
+      statusCode: 200,
+      body: JSON.stringify({
+        message: JSON.stringify(reply)
+      }),
+    };
+    callback(null, response);
+  }; 
